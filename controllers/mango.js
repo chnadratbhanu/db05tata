af82088fc04b3c8aa10414be11e2d4b82809b939
CHANDRA BHANU TATA <TATACHANDRABHANU1141@GMAIL.COM>
2021-04-13 23:08:03 -0500

    added endpoints

--- controllers/mango.js
+++ controllers/mango.js
@@ -1,4 +1,4 @@
-const mango = require('../models/mango');
+// const mango = require('../models/mango');
 var Mango = require('../models/mango');
 // List of all mangos
 exports.mango_list = async function(req, res) {
@@ -40,10 +40,7 @@ exports.mango_create_post = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
-// Handle mango delete form on DELETE.
-exports.mango_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: mango delete DELETE ' + req.params.id);
-};
+
 // Handle mango update form on PUT.
 exports.mango_update_put = async function(req, res) {
     console.log(`update on id ${req.params.id} with body 
@@ -75,3 +72,74 @@ exports.mango_view_all_Page = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
+// Handle Costume delete on DELETE.
+exports.mango_delete = async function(req, res) {
+    console.log("delete "  + req.params.id)
+    try {
+        result = await Mango.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.mango_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await Mango.findById( req.query.id)
+        res.render('mangodetail', 
+{ title: 'Mango Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.mango_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('mangocreate', { title: 'Mango Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.mango_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await Mango.findById(req.query.id)
+        console.log(result)
+        res.render('mangoupdate', { title: 'mango Update', toShow: result });
+        // console.log(toShow)
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.mango_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await Mango.findById(req.query.id)
+        res.render('mangodelete', { title: 'mango Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
--- routes/mango.js
+++ routes/mango.js
@@ -3,4 +3,18 @@ const mango_controlers= require('../controllers/mango');
 var router = express.Router();
 /* GET costumes */
 router.get('/', mango_controlers.mango_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', mango_controlers.mango_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', mango_controlers.mango_create_Page);
+
+/* GET create update page */
+router.get('/update', mango_controlers.mango_update_Page);
+
+/* GET create delete page */
+router.get('/delete', mango_controlers.mango_delete_Page);
+
+
+
 module.exports = router;
